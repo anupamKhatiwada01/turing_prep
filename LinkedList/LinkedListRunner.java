33c63a2650d6247c4ca99c29a670eeedec1b177a
anupamKhatiwada <anupamkhatiwada01@gmail.com>
2023-04-05 06:07:27 +0000

    .

--- LinkedList/LinkedListRunner.java
+++ LinkedList/LinkedListRunner.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 
 class LinkedListRunner {
 
@@ -7,8 +8,6 @@ class LinkedListRunner {
     Node headNode = new Node();
     headNode.setValue(15);
     newList.setHead(headNode);
-
-    // System.out.println(newList.getHead().getValue());
 
     // Let's add a list of integer values to the linked list
     Integer[] inputList = { 25, 14, 15, 17, 19, 25, 32, 87, 91 };
@@ -25,20 +24,54 @@ class LinkedListRunner {
     }
 
     // Checking elements present in the linked list
-    currentNode = newList.getHead();
-    current = 0;
-    while (current <= inputList.length) {
-      System.out.println(currentNode.getValue());
-      currentNode = currentNode.getNext();
-      current++;
-    }
+    // newList = removeDuplicates(newList);
+    // currentNode = newList.getHead();
+
+    // while(currentNode.getNext()!=null){
+    // System.out.println(currentNode.getValue());
+    // currentNode = currentNode.getNext();
+    // }
+    // System.out.println(currentNode.getValue());
+
+    System.out.println(kFromLast(newList, 3).getValue());
 
   }
 
+  static Node kFromLast(LinkedList input, Integer k) {
+    Node foundNode = null;
+    Node current = input.getHead();
+
+    while (current.getNext() != null) {
+      
+      current = current.getNext();
+      if (k==0) foundNode = input.getHead();
+      if(foundNode!=null) foundNode = foundNode.getNext();
+      k--;
+    }
+
+    return foundNode;
+  }
+
   static LinkedList removeDuplicates(LinkedList input) {
+    if (input.getHead() == null || input.getHead().getNext() == null)
+      return input;
+    HashMap<Integer, Boolean> refMap = new HashMap<>();
 
+    Node previous = null;
+    Node current = input.getHead();
 
-    
+    while (current.getNext() != null) {
+      if (!refMap.containsKey(current.getValue())) {
+        refMap.put(current.getValue(), true);
+      } else {
+        previous.setNext(current.getNext());
+      }
+      previous = current;
+      current = current.getNext();
+    }
+
+    // System.out.println(current.getValue());
+    // System.out.println(previous.getValue());
 
     return input;
   }
@@ -49,13 +82,13 @@ class Node {
   private Node next = null;
   // Make sure whatever object is return has an explicit hash code and equals
   // method defined
-  private Object value = null;
+  private Integer value = null;
 
   void setNext(Node inputNode) {
     next = inputNode;
   }
 
-  void setValue(Object inputValue) {
+  void setValue(Integer inputValue) {
     value = inputValue;
   }
 
@@ -63,7 +96,7 @@ class Node {
     return next;
   }
 
-  Object getValue() {
+  Integer getValue() {
     return value;
   }
 }
