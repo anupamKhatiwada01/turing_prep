1d36ceeb5b91284ec0c68dc8b34251317fe8f6d0
anupamKhatiwada <anupamkhatiwada01@gmail.com>
2023-04-20 12:57:42 +0000

    finally started working through stack and queue problems

--- Stacks_and_Queues/StackRunner.java
+++ Stacks_and_Queues/StackRunner.java
@@ -2,35 +2,55 @@ public class StackRunner {
 
   public static void main(String[] args) {
 
-    Stack stack = new Stack(10);
-    stack.add(15);
-    stack.add(25);
-    stack.add(30);
+    // Stack stack = new Stack(13);
+    // stack.push(15);
+    // stack.push(12);
+    // stack.push(30);
+    // stack.push(25);
 
-    try {
-      stack.pop();
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
+    // while (!stack.isEmpty()) {
+    // try {
 
-    while (!stack.isEmpty()) {
-      // System.out.println(stack.peek());
-      try {
-        System.out.println(stack.pop());
-      } catch (Exception e) {
-        System.out.println(e.getMessage());
-      }
+    // // System.out.println(stack.peek());
 
-    }
-    // System.out.println(stack.add(15));
-    // System.out.println(stack.peek());
-    try {
-      System.out.println(stack.pop());
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }
-    // System.out.println(stack.isEmpty());
+    // System.out.println("minimum: "+stack.getMin());
+    // // System.out.println(stack.pop());
+    // stack.pop();
+    // } catch (Exception e) {
+    // System.out.println(e.getMessage());
+    // }
 
+    // }
+
+  }
+
+}
+
+// inputArr is the array to be used to implement the stacks.
+// x is the number of stacks that are to be implemented.
+// The question specifically asks for 3 but makes sense to implement it more
+// generically.
+// To do this we will pass in the array which will be passed by reference so we
+// can use the stored data
+// I feel it doesn't make sense to do it in a method. Seems like I should
+// implement a new class ArrayStack that implements multiple
+// stacks and intenally uses an array to do stuff.
+class ArrayStack {
+  // inputArray is the Array used to implement the stack
+  // StackArray is the array used to maintain the counters.
+  // It's length will be equal to the stackNumber ie. the number of stacks to be
+  // implemented.
+  private Integer[] inputArray;
+  private Integer stackNumber;
+  private int[] stackArray;
+
+  // The constructor will take an Integer for stack implementation
+  // We need to set up as many counters as there are stack numbers
+  // How will the addition/deletion to a particular stack work?
+  public ArrayStack(Integer[] arr, Integer stackNumber) {
+    this.inputArray = arr;
+    this.stackNumber = stackNumber;
+    this.stackArray = new int[stackNumber];// Stack array whose length will be equal to the stack number
   }
 
 }
@@ -47,14 +67,11 @@ class Stack {
   private class Node {
     private Integer value = null;
     private Node next = null;
+    private Integer min = null;
 
     public Node(Integer input) {
       this.value = input;
     }
-
-    // public void setValue(Integer input){
-    // this.value = input;
-    // }
 
     public void setNext(Node input) {
       this.next = input;
@@ -72,6 +89,15 @@ class Stack {
       return this.next;
     }
 
+    // For minimum
+    public void setMin(Integer input) {
+      this.min = input;
+    }
+
+    public Integer getMin() {
+      return this.min;
+    }
+
   }
 
   private Node head;
@@ -85,13 +111,26 @@ class Stack {
   }
 
   public Stack(Integer input) {
-    this.head = new Node(input);
+    // this.head = new Node(input);
+    this.push(input);
   }
 
-  public Boolean add(Integer input) {
+  public Boolean push(Integer input) {
     try {
       Node toAdd = new Node(input);
+      // If the head is null the below line will automatically set the next of the
+      // node being added as null
       toAdd.setNext(this.getHead());
+      // Set the minimum till that point on the node when its pushed
+      // Done in this manner we don't need to touch pop method for setting minimum
+      if (this.isEmpty() == true)
+        toAdd.setMin(input);
+      else {
+        if (this.getHead().getMin() >= input)
+          toAdd.setMin(input);
+        else
+          toAdd.setMin(this.getHead().getMin());
+      }
       this.setHead(toAdd);
       return true;
     } catch (Exception e) {
@@ -100,8 +139,9 @@ class Stack {
   }
 
   public Integer pop() throws Exception {
-    if (this.getHead() == null)
+    if (this.getHead() == null) {
       throw new Exception("The stack is empty");
+    }
     Integer value = this.getHead().getValue();
     if (this.getHead().getNext() != null) {
       this.setHead(this.getHead().getNext());
@@ -110,8 +150,16 @@ class Stack {
     return value;
   }
 
-  public Integer peek() {
+  public Integer peek() throws Exception {
+    if (this.isEmpty())
+      throw new Exception("The stack is empty.");
     return this.getHead().getValue();
+  }
+
+  public Integer getMin() throws Exception {
+    if (this.isEmpty())
+      throw new Exception("The stack is empty.");
+    return this.getHead().getMin();
   }
 
   public boolean isEmpty() {
